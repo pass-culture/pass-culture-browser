4616255f059708043cf68f6a16fcecaec097ef4b
erwanledoux <lawanledoux@gmail.com>
2018-01-24 19:12:20 +0100

    added withSelectors hoc

--- src/components/OfferCard.js
+++ src/components/OfferCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import createSelector from 'reselect'
 
 import Icon from './Icon'
+import withSelectors from '../hocs/withSelectors'
 import { API_URL } from '../utils/config'
 
-const OfferCard = ({ id,
+const OfferCard = ({ bargainPrices,
+  id,
   name,
-  prices,
   sellersFavorites,
+  sortedPrices,
   work
 }) => {
   return (
@@ -15,14 +18,14 @@ const OfferCard = ({ id,
         className='offerPicture'
         src={`${API_URL}/thumbs/${work.id}`} />
       {
-        sellersFavorites && sellersFavorites.length>0 &&
+        sellersFavorites && sellersFavorites.length > 0 &&
         <Icon name='favorite-outline' />
       }
       {
-        prices.filter(p => p.groupSize>1).length>0 &&
+        bargainPrices && bargainPrices.length > 0 &&
         <Icon name='error' />
       }
-      { prices.sort((p1, p2) => p1.value > p2.value)[0].value }&nbsp;€&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; à {(20-id)*15}m
+      { sortedPrices && sortedPrices[0].value }&nbsp;€&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; à {(20-id)*15}m
       <div className='offerName'>
         { name || work.name }
       </div>
@@ -30,4 +33,13 @@ const OfferCard = ({ id,
   )
 }
 
-export default OfferCard
+export default withSelectors({
+  bargainPrices: [
+    ({ prices }) => prices,
+    prices => prices.filter(p => p.groupSize>1)
+  ],
+  sortedPrices: [
+    ({ prices }) => prices,
+    prices => prices.sort((p1, p2) => p1.value > p2.value)
+  ]
+})(OfferCard)
