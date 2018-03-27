dd08a8600b047bd296003f7e79ad18f8a56cc02b
erwanledoux <lawanledoux@gmail.com>
2018-03-27 15:47:13 +0200

    added disabling for hidden case

--- src/components/UserMediationsDebug.js
+++ src/components/UserMediationsDebug.js
@@ -9,10 +9,9 @@ const UserMediationsDebug = ({ afterLimit,
   isLoadingBefore,
   userMediations
 }) => {
-
   return (
     <div className='user-mediations-debug absolute left-0 ml2 p2'>
-        ({isLoadingBefore ? '?' : ' '}{beforeLimit + 1}) {aroundIndex + 1} ({afterLimit + 1} {isLoadingAfter ? '?' : ' '}) / {userMediations.length}
+        ({isLoadingBefore ? '?' : ' '}{beforeLimit + 1}) {aroundIndex + 1} ({afterLimit + 1} {isLoadingAfter ? '?' : ' '}) / {userMediations.length + 1}
     </div>
   )
 }
