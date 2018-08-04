96f66502ef0ff1ac5c5cb98ab16867130acf9611
erwanledoux <lawanledoux@gmail.com>
2018-08-04 19:23:15 +0200

    added redux-persist

--- src/Root.js
+++ src/Root.js
@@ -8,8 +8,10 @@ import {
   Route,
   Switch,
 } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
+import persistor from './utils/persistor'
 import routes from './utils/routes'
 import store from './utils/store'
 import { API_URL, IS_DEV } from './utils/config'
@@ -25,36 +27,38 @@ const Root = () => {
   }
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App>
-          <Switch>
-            {routes.map(route => (
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App>
+            <Switch>
+              {routes.map(route => (
+                <Route
+                  key={route.path}
+                  {...route}
+                  render={match => {
+                    document.title = `${
+                      route.title ? `${route.title} - ` : ''
+                    }Pass Culture`
+                    return route.render(match)
+                  }}
+                />
+              ))}
               <Route
-                key={route.path}
-                {...route}
-                render={match => {
-                  document.title = `${
-                    route.title ? `${route.title} - ` : ''
-                  }Pass Culture`
-                  return route.render(match)
+                path="/:active?"
+                render={({ match, location }) => {
+                  const matchedRoute = routes.find(route =>
+                    matchPath(`/${match.params.active}`, route)
+                  )
+                  return (
+                    location.pathname !== '/' &&
+                    !matchedRoute && <Redirect to="/" />
+                  )
                 }}
               />
-            ))}
-            <Route
-              path="/:active?"
-              render={({ match, location }) => {
-                const matchedRoute = routes.find(route =>
-                  matchPath(`/${match.params.active}`, route)
-                )
-                return (
-                  location.pathname !== '/' &&
-                  !matchedRoute && <Redirect to="/" />
-                )
-              }}
-            />
-          </Switch>
-        </App>
-      </BrowserRouter>
+            </Switch>
+          </App>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   )
 }
