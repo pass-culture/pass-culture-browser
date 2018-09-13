89f6979fa329b1311cdc05dd34b411514874692b
Anne-Marie Esteves <amesteves@octo.com>
2018-09-13 15:37:40 +0200

    refacto with a select by types component that will be added with a different design in search page, discovery page and filter menu

---
+++ src/components/SearchByOfferType.js
@@ -0,0 +1,100 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const SearchByOfferType = ({ handleQueryParamsChange, title }) => (
+  <div>
+    <h2>
+      {title}
+    </h2>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Applaudir
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Applaudir' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Découvrir
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Découvrir' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Ecouter
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Ecouter' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Jouer
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Jouer' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Lire
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Lire' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Pratiquer
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Pratiquer' })}
+        type="checkbox"
+      />
+    </div>
+    <div className="field checkbox">
+      <label id="from_date" className="label">
+        {' '}
+        Regarder
+      </label>
+      <input
+        id="from_date"
+        className="input is-normal"
+        onChange={() => handleQueryParamsChange({ type: 'Regarder' })}
+        type="checkbox"
+      />
+    </div>
+  </div>
+)
+
+SearchByOfferType.propTypes = {
+  handleQueryParamsChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+}
+export default SearchByOfferType
