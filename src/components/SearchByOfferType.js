db05ff0c6133f962af732d82d2b8fa56fb0f2b6b
Anne-Marie Esteves <amesteves@octo.com>
2018-09-18 17:21:55 +0200

    add clear search by text filter, still wip

--- src/components/SearchByOfferType.js
+++ src/components/SearchByOfferType.js
@@ -7,84 +7,84 @@ const SearchByOfferType = ({ handleQueryParamsChange, title }) => (
       {title}
     </h2>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Applaudir
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Applaudir' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Découvrir
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Découvrir' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Ecouter
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Écouter' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Jouer
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Jouer' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Lire
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Lire' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Pratiquer
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Pratiquer' })}
         type="checkbox"
       />
     </div>
     <div className="field checkbox">
-      <label id="from_date" className="label">
+      <label id="type" className="label">
         {' '}
         Regarder
       </label>
       <input
-        id="from_date"
+        id="type"
         className="input is-normal"
         onChange={() => handleQueryParamsChange({ type: 'Regarder' })}
         type="checkbox"
