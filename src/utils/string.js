0195b220c289bf4213a8cb5ef390f5b1aff6a71e
erwanledoux <lawanledoux@gmail.com>
2018-09-19 17:19:53 +0200

    used frenchKeywordsString

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,29 +1,42 @@
+/* eslint-disable */
+/*
+import { queryStringToObject, objectToQueryString } from 'pass-culture-shared'
+
 // eslint-disable prefer-const
-function queryStringToObject(string = '') {
-  return string
-    .replace(/^\??/, '')
-    .split('&')
-    .filter(el => el)
-    .reduce((result, group) => {
-      let [key, value] = group.split('=') // eslint-disable-line
-      switch (key) {
-        case 'date':
-          key = 'eventOccurrenceIdOrNew'
-          break
-        case 'stock':
-          key = 'stockIdOrNew'
-          break
-        case 'lieu':
-          key = 'venueId'
-          break
-        case 'structure':
-          key = 'offererId'
-          break
-        default:
-          break
-      }
-      return Object.assign({}, result, { [key]: value })
-    }, {})
+export function frenchQueryStringToEnglishObject(string = '') {
+  const obj = queryStringToObject(string)
+
+  console.log('frenchQueryStringToEnglishObject', obj)
+
+  Object.keys(obj).forEach(key => {
+    switch (key) {
+      case 'mots-cles':
+        obj.keywords = obj[key]
+        delete obj[key]
+        break
+      default:
+        break
+    }
+  })
+
+  console.log('frenchQueryStringToEnglishObject after', obj)
+
+  return obj
 }
 
-export default queryStringToObject
+export function englishObjectToFrenchQueryString(obj) {
+
+  console.log('englishObjectToFrenchQueryString', obj)
+  let string = objectToQueryString(obj)
+
+  string = string.replace(/keywords=/, 'mots-cles=')
+
+  return string
+}
+*/
+
+export function frenchQueryStringToEnglishQueryString(string) {
+  string = string.replace(/mots-cles=/, 'keywords=')
+
+  return string
+}
