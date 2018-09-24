1ec3f3e938bb55b25161c449151a0b5b54c5b2c7
sixertoy <sixertoy.github@gmail.com>
2018-09-24 09:45:33 +0200

    🚓 ajout du dossier public/static/fontello dans le gitignore, creation d'une commande pour download les fonts au start

---
+++ scripts/fontello.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# ------
+# Download Fontello CSS/Font Files to public folder
+# @see https://github.com/ryantsangai/fontello-cli
+#
+CONFIG_PATH='./.fontello.json'
+SESSION_PATH='./.fontello.session'
+OUTPUT_BASE_FOLDER='./public/static/fontello'
+
+./node_modules/.bin/fontello install --config $CONFIG_PATH --session $SESSION_PATH --css $OUTPUT_BASE_FOLDER/css --font $OUTPUT_BASE_FOLDER/font
