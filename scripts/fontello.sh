122087879f948c6ac8d52e66c04228d20348a04a
sixertoy <sixertoy.github@gmail.com>
2018-10-01 18:08:11 +0200

    🚓 update font configuration with invision icons

--- scripts/fontello.sh
+++ scripts/fontello.sh
@@ -4,7 +4,7 @@
 # Download Fontello CSS/Font Files to public folder
 # @see https://github.com/ryantsangai/fontello-cli
 #
-CONFIG_PATH='./.fontello.json'
+CONFIG_PATH='./fontello.json'
 SESSION_PATH='./.fontello.session'
 OUTPUT_BASE_FOLDER='./public/static/fontello'
 
