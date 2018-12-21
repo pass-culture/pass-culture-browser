811b68f308cfbf8518d46eaefb8b42130267d7d3
sixertoy <sixertoy.github@gmail.com>
2018-12-21 11:14:50 +0100

    pc-1293 changer wording page beta

--- src/components/pages/BetaPage.js
+++ src/components/pages/BetaPage.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 import { withLogin } from 'pass-culture-shared'
 
-const BetaPage = () => (
+export const RawBetaPage = () => (
   <div id="beta-page" className="page pc-gradient flex-rows">
     <main role="main" className="pc-main padded flex-rows flex-center flex-0">
       <h1 className="text-left fs32">
         <span className="is-bold is-italic is-block">Bienvenue</span>
         <span className="is-italic is-block is-semi-bold">
-          dans la version beta
+          dans l&apos;avant-première
         </span>
         <span className="is-italic is-block">du Pass Culture</span>
       </h1>
@@ -32,11 +32,11 @@ const BetaPage = () => (
         <span
           aria-hidden="true"
           className="pc-icon icon-legacy-next-long"
-          title="C&apos;est par là"
+          title="C'est par là"
         />
       </Link>
     </footer>
   </div>
 )
 
-export default withLogin({ successRedirect: '/decouverte' })(BetaPage)
+export default withLogin({ successRedirect: '/decouverte' })(RawBetaPage)
