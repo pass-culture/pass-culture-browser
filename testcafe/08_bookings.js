fc23341d41b3d2ac33c4dc18335d4fe839c22b14
sixertoy <sixertoy.github@gmail.com>
2019-02-06 09:40:00 +0100

    PC-1448 move all getPageUrl in helpers

--- testcafe/08_bookings.js
+++ testcafe/08_bookings.js
@@ -1,6 +1,25 @@
-import { ROOT_PATH } from '../src/utils/config'
+// ./node_modules/.bin/testcafe chrome ./testcafe/08_bookings.js
+// import { Selector } from 'testcafe'
 
-fixture('08_01 Booking Card').page(`${ROOT_PATH}connexion`)
+import getPageUrl from './helpers/getPageUrl'
+import { ROOT_PATH } from '../src/utils/config'
+import { createUserRole } from './helpers/roles'
+import { hasSignedUpUser93 } from './helpers/users'
+
+// const bookingVersoButton = Selector('#open-menu-button')
+
+fixture('08_01 Booking Card').before(async t => {
+  await t
+    .useRole(createUserRole(hasSignedUpUser93))
+    .navigateTo(`${ROOT_PATH}decouverte/KU/LE/verso`)
+    .wait(100)
+})
+
+test('Je suis redirigé vers la page /decouverte/KU/LE/verso', async t => {
+  await t
+    .expect(getPageUrl())
+    .contains('/decouverte/KU/LE/verso', { timeout: 500 })
+})
 
 // J'accède à une offre
 // Je clique sur le bouton fleche/details
