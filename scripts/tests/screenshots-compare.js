b10ac6d505316da6d55ace16182834afda1733ef
erwanledoux <lawanledoux@gmail.com>
2019-06-04 18:25:18 +0200

    upgraded testcafe

--- scripts/tests/screenshots-compare.js
+++ scripts/tests/screenshots-compare.js
@@ -2,7 +2,6 @@
   import/no-extraneous-dependencies: 0 */
 import path from 'path'
 import fse from 'fs-extra'
-import 'moment-duration-format'
 import parseArgs from 'minimist'
 import resemble from 'resemblejs'
 
