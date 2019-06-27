8a921925c43c32aa870c70311070171e8751bf56
erwanledoux <lawanledoux@gmail.com>
2019-06-27 13:36:32 +0200

    (pC-2049) fixed snapshot

--- src/reducers/tests/data.spec.js
+++ src/reducers/tests/data.spec.js
@@ -6,6 +6,7 @@ describe('src | reducers | data  ', () => {
     const action = {}
     const expected = {
       bookings: [],
+      features: null,
       readRecommendations: [],
       recommendations: [],
       types: [],
