e5cfc566a80cdced2bb09c7682a28d7198ed7da0
Fabien Mercier <fmercier@octo.com>
2020-04-06 09:33:05 +0200

    (pC-3206) fix feature flipping on route

--- src/components/router/routes.jsx
+++ src/components/router/routes.jsx
@@ -79,7 +79,6 @@ const routes = [
    ---------------------------------------------------  */
   {
     component: DiscoveryRouterContainer,
-    featureName: 'RECOMMENDATIONS_WITH_DISCOVERY_VIEW',
     icon: 'ico-offres',
     path:
       '/decouverte' +
