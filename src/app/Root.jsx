2ed9b4a16c01f566521860a8798542535bee9515
Emma Jamin <emmajamin@orange.fr>
2021-07-29 16:44:43 +0200

    wip

--- src/app/Root.jsx
+++ src/app/Root.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
+import SmartBanner from 'react-smartbanner'
 
 import PageNotFoundContainer from '../components/layout/ErrorBoundaries/ErrorsPage/PageNotFound/PageNotFoundContainer'
 import MatomoContainer from '../components/matomo/MatomoContainer'
@@ -20,6 +21,14 @@ const Root = () => (
         loading={null}
         persistor={persistor}
       >
+        <SmartBanner
+          button="Voir"
+          daysHidden={1}
+          position="top"
+          price={{ ios: 'GRATUIT', android: 'GRATUIT' }}
+          storeText={{ ios: "Sur l'App Store", android: 'Sur Google Play' }}
+          title="pass Culture"
+        />
         <BrowserRouter>
           <AppContainer>
             <Switch>
